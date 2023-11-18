6ca7f5663e41b8facee9b72268d34ee5e07e4f7b
w!chu <wichu.mgmt@gmail.com>
2023-11-18 15:18:23 +0200

    б

--- exam.py
+++ exam.py
@@ -24,53 +24,56 @@ with open("vinyl1.txt", "a", encoding="utf-8") as file:
 
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, "lxml")
-            all_products = soup.find_all("div", class_="products-layout__item without-options-1 without-options-3 without-options-4")
+            all_products = soup.find_all("div", class_="product-card")
 
             for product in all_products:
-                if product.find("div", class_="product-card"):
-                    title = product.find("a", class_="product-card__title")
-                    review = product.find("span", class_="comments-preview__count")
-                    price = product.find("span", class_="sum")
-                    print(title.text, review.text, price.text)
-                    # price = price.text.replace(" ", " ")
-                    file.write(f"{title.text} {review.text} {price.text}\n")
-                    sheet1[f"A{count}"] = title.text
-                    sheet1[f"B{count}"] = review.text
-                    sheet1[f"C{count}"] = price.text
-                    count += 1
+                title = product.find("a", class_="product-card__title")
+                try:
+                    review = product.find("span", class_="comments-preview__count").text
+                except AttributeError:
+                    review = 0
+                price = product.find("div", class_="v-pb")
+                print(title.text, review, price.text)
+                file.write(f"{title.text} {review} {price.text}\n")
+                sheet1[f"A{count}"] = title.text
+                sheet1[f"B{count}"] = review
+                sheet1[f"C{count}"] = price.text
+                count += 1
 
             book.save("Catalog1.xlsx")
             book.close()
 
-book = openpyxl.Workbook()
-sheet2 = book.active
+book1 = openpyxl.Workbook()
+sheet2 = book1.active
 
 sheet2["A1"] = "Title"
 sheet2["B1"] = "Reviews"
 sheet2["C1"] = "With discounts"
 
 with open("vinyl2.txt", "a", encoding="utf-8") as file:
-    for j in range(1, 26):
-        print(f"Page = {j}")
-        url = f"https://allo.ua/ua/vinilovye-proigryvateli/p-={j}"
+    for b in range(1, 26):
+        print(f"Page = {b}")
+        url = f"https://allo.ua/ua/vinilovye-proigryvateli/p-={b}"
         response = session.get(url, headers=header)
 
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, "lxml")
-            all_products = soup.find_all("div", class_="products-layout__item without-options-1 without-options-3 without-options-4")
+            all_products = soup.find_all("div", class_="product-card")
 
             for product in all_products:
-                if product.find("div", class_="v-pb"):
+                if product.find("div", class_="v-pb__old"):
                     title = product.find("a", class_="product-card__title")
-                    review = product.find("span", class_="comments-preview__count")
-                    price = product.find("span", class_="sum")
-                    print(title.text, review.text, price.text)
-                    # price = price.text.replace(" ", " ")
-                    file.write(f"{title.text} {review.text} {price.text}\n")
+                    try:
+                        review = product.find("span", class_="comments-preview__count").text
+                    except AttributeError:
+                        review = 0
+                    price = product.find("div", class_="v-pb__cur discount")
+                    print(title.text, review, price.text)
+                    file.write(f"{title.text} {review} {price.text}\n")
                     sheet2[f"A{count}"] = title.text
-                    sheet2[f"B{count}"] = review.text
+                    sheet2[f"B{count}"] = review
                     sheet2[f"C{count}"] = price.text
                     count += 1
 
-            book.save("Catalog2.xlsx")
-            book.close()
+            book1.save("Catalog2.xlsx")
+            book1.close()
